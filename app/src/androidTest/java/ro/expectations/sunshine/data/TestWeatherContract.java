79a3d4e561178ad02d19993567d658cc2ae28e65
Adrian Videnie <avidenie@gmail.com>
2015-08-12 22:27:30 +0300

    Add content provider and associated unit tests

---
+++ app/src/androidTest/java/ro/expectations/sunshine/data/TestWeatherContract.java
@@ -0,0 +1,24 @@
+package ro.expectations.sunshine.data;
+
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+/*
+    Students: This is NOT a complete test for the WeatherContract --- just for the functions
+    that we expect you to write.
+ */
+public class TestWeatherContract extends AndroidTestCase {
+
+    // intentionally includes a slash to make sure Uri is getting quoted correctly
+    private static final String TEST_WEATHER_LOCATION = "/North Pole";
+    private static final long TEST_WEATHER_DATE = 1419033600L;  // December 20th, 2014
+
+    public void testBuildWeatherLocation() {
+        Uri locationUri = WeatherContract.WeatherEntry.buildWeatherLocation(TEST_WEATHER_LOCATION);
+        assertEquals("Error: Weather location not properly appended to the end of the Uri",
+                TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
+        assertEquals("Error: Weather location Uri doesn't match our expected result",
+                locationUri.toString(),
+                "content://ro.expectations.sunshine.weather.provider/weather/%2FNorth%20Pole");
+    }
+}
